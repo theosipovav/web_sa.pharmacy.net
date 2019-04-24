a6af070a8ee5bea8eded43370f7759d580862d90
Aleksey <theosipovav@gmail.com>
2019-04-24 22:30:54 +0300

    parser

--- app/controllers/scan.php
+++ app/controllers/scan.php
@@ -1,10 +1,26 @@
 <?php
 require_once("../lib/parser/simple_html_dom.php");
 
-$url1 = 'https://366.ru/c/lekarstva/?page=1&q=%3Apriority-desc';
+$url1 = 'https://366.ru/c/lekarstva/?page=0&q=%3Apriority-desc';
 
 @$site_parsing = file_get_html($url1);
-$site_parsing_tables = $site_parsing -> find('div.b-bckgr--common');
+$site_parsing_tables = $site_parsing -> find('div.c-prod-item-list div.c-prod-item');
 
+foreach($site_parsing_tables as $item) {
+    $a1 = $item -> find('h3.c-prod-item__title', 0)->plaintext;
+    $a2 = $item -> find('div.c-prod-item__manufacturer div', 0)->plaintext;
+    $a3 = $item -> find('div.c-prod-item__manufacturer div', 1)->plaintext;
+    $a4 = $item -> find('div.c-prod-item__manufacturer div', 2)->plaintext;
 
-var_dump($site_parsing_tables);
+    
+    echo "Наименование: ". $a1;
+    echo "<br>";
+    echo "Компания: " . $a2;
+    echo "<br>";
+    echo "Страна: " . $a3;
+    echo "<br>";
+    echo "Категория: " . $a4;
+
+    echo "<hr/>";
+    break;
+}
--- app/views/content_listres.php
+++ app/views/content_listres.php
@@ -19,13 +19,14 @@
                 <tr>
                     <th scope="row">1</th>
                     <td>
-                        www.eapteka.ru
+                        www.366.ru/
                     </td>
                     <td class="text-right">
                         21-04-2019
                     </td>
                     <td class="text-right">
-                        <a href="https://www.eapteka.ru/goods/drugs/" target="_blank" class="btn btn-outline-light ">На сайт</a>
+                        <a href="https://366.ru/c/lekarstva/?page=0&q=%3Apriority-desc" target="_blank"
+                            class="btn btn-outline-light ">На сайт</a>
                         <a href="app/controllers/scan.php" target="_blank" class="btn btn-light">Проанализировать</a>
                     </td>
                 </tr>
