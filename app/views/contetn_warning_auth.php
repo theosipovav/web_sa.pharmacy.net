05540b632e2d6c6a8d817af754ef2e70341e05d7
theOsipovAV <theosipovav@gmail.com>
2019-04-25 22:21:05 +0300

    Создание стр. - о сервисе

---
+++ app/views/contetn_warning_auth.php
@@ -0,0 +1,20 @@
+<div class="content">
+    <div class="container">
+        <hr />
+        <div class="row">
+            <div class="col">
+                <div class="card text-white bg-warning mb-3">
+                    <div class="card-header">Внимание!</div>
+                    <div class="card-body">
+                        <h4 class="card-title">Вы не авторизованы</h4>
+                        <p class="card-text text-center">Для продолжения работы необходимо<br><a href="login.php"
+                                class="btn btn-light">войти под учетной записью</a><br>или<br><a href="registration.php"
+                                class="btn btn-light">зарегистрировать новую учетную запись</a>
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
