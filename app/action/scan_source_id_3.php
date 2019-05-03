1295e769b0a3d364fdf9ca8cc01f4341169a5a6d
theosipovav <theosipovav@gmail.com>
2019-05-03 08:59:57 +0300

    Парсинг для 2го сайта

--- app/action/scan_source_id_3.php
+++ app/action/scan_source_id_3.php
@@ -3,10 +3,11 @@ ini_set('MAX_EXECUTION_TIME', '-1');
 
 require_once("../lib/parser/simple_html_dom.php");
 
-$nSourceId = 2;
+$nSourceId = 3;
 $dateCurrent = date('Y-m-d H:i:s');
 $nLogId = 0;
 
+/*
 $pdoConnection = new PDO('mysql:host=localhost;dbname=sa.pharmacy.net', 'administrator', '611094');
 $pdoQuery = "INSERT INTO `scan_log` (`id`, `source_id`, `date`) VALUES (NULL, '$nSourceId', '$dateCurrent');";
 $pdoRes = $pdoConnection->query($pdoQuery);
@@ -15,7 +16,8 @@ if ($pdoRes == false)
     print("-1");
     exit();
 }
-
+*/
+/*
 $pdoQuery = "SELECT id FROM `scan_log` ORDER BY id DESC LIMIT 1";
 $pdoRes = $pdoConnection->query($pdoQuery);
 $pdoResArray = $pdoRes->fetchAll();
@@ -25,23 +27,22 @@ if ($nLogId == "")
     print("-1");
     exit();
 }
+*/
 $nPageNum = 1;
 $count = 0;
 while (true)
 {
-    $sUrlParsing = 'https://apteka.ru/catalog/section/lekarstvennye-preparaty/?PAGEN_1=' . $nPageNum;
-    @$site_parsing = file_get_html($sUrlParsing);
-    if ($site_parsing == null)
-    {
-        break;
-    }
-    else
-    {
-        $nPageNum++;
-    }
-    $htmlCatalogList = $site_parsing -> find('div.catalog-list', 0);
+    $sUrlParsing = 'https://vseapteki.ru/catalog/39-lekarstva_i_bad/?page=' . $nPageNum;
+    @$htmlPage = file_get_html($sUrlParsing);
+
     $aHtmlArticle = $htmlCatalogList-> find('article');
-    foreach ($aHtmlArticle as $article) {
+    echo count($aHtmlArticle);
+
+    exit();
+    $htmlCatalogList = $htmlPage -> find('div.catalog-list', 0);
+    $aHtmlArticle = $htmlCatalogList-> find('article');
+    foreach ($aHtmlArticle as $article) 
+    {
         $sName = $article->find('span.h2-style',0)->children(0)->plaintext;
         $nPriceData = $article->find('div.price',0);
         if (isset($nPriceData))
@@ -77,5 +78,6 @@ while (true)
             $count++;
         }
     }
+    break;
 }
 print($count);
